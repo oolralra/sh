96768973a714b24f34244e2f5734f01da2145f15
pcmin929 <120066709+pcmin929@users.noreply.github.com>
2023-04-07 14:08:12 +0900

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-apt-get update -y
-apt-get install -y openjdk-11-jdk
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FCEF32E745F2C3D5
-sudo apt-get -y update
-sudo apt-get -y install jenkins
+
+sudo apt -y update
+
+sudo apt -y install openjdk-11-jre
+
+java -version
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt -y update
+
+sudo apt -y install jenkins
+
 sed -i s/HTTP_PORT=8080/HTTP_PORT=7777/g /etc/default/jenkins
+
 sed -i s/JENKINS_PORT=8080/JENKINS_PORT=7777/g /usr/lib/systemd/system/jenkins.service
+
 systemctl daemon-reload
+
 systemctl restart jenkins
+
 systemctl enable jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+cat /var/lib/jenkins/secrets/initialAdminPassword
